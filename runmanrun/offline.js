9a38c0447ea1e1d5a41391004f83875155e82c86
tawatchai-6898 <69179193+tawatchai-6898@users.noreply.github.com>
2021-10-07 14:24:42 +0700

    runrun2

--- runmanrun/offline.js
+++ runmanrun/offline.js
@@ -1,16 +1,16 @@
 ﻿{
-	"version": 1633589818,
+	"version": 1633591438,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/player-sheet0.png",
-		"images/bg-sheet0.png",
 		"images/fruit-sheet0.png",
 		"images/playbutton-sheet0.png",
 		"images/sprite_r-sheet0.png",
 		"images/replay-sheet0.png",
+		"images/tiledbackground.png",
 		"media/item1.ogg",
 		"media/potc01.m4a",
 		"media/potc01.ogg",
